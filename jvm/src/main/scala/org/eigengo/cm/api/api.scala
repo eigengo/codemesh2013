5b9a3d5ae6f0667675523891c15171640abfbc6d
Jan Machacek <jan.machacek@gmail.com>
2013-12-04 16:35:39 +0000

    Layers

--- jvm/src/main/scala/org/eigengo/cm/api/api.scala
+++ jvm/src/main/scala/org/eigengo/cm/api/api.scala
@@ -3,4 +3,8 @@ package org.eigengo.cm.api
 import akka.actor.Props
 import spray.can.Http
 import akka.io.IO
+import org.eigengo.cm.core.Core
 
+trait Api {
+  this: Core =>
+}
--- jvm/src/main/scala/org/eigengo/cm/core/core.scala
+++ jvm/src/main/scala/org/eigengo/cm/core/core.scala
@@ -3,3 +3,16 @@ package org.eigengo.cm.core
 import akka.actor.{ Props, ActorSystem }
 import com.rabbitmq.client.ConnectionFactory
 import com.github.sstone.amqp.ConnectionOwner
+
+trait Core {
+
+  implicit lazy val system = ActorSystem("recog")
+
+  val amqpConnectionFactory = new ConnectionFactory()
+  amqpConnectionFactory.setHost("localhost")
+
+  lazy val amqpConnection = system.actorOf(Props(new ConnectionOwner(amqpConnectionFactory)))
+
+  lazy val coordinator = system.actorOf(Props(new CoordinatorActor(amqpConnection)), "coordinator")
+
+}
