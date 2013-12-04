427b2039a23f4087ae072b671b434ca1ba519af9
Jan Machacek <jan.machacek@gmail.com>
2013-12-04 16:36:13 +0000

    Core configuration

--- jvm/src/main/scala/org/eigengo/cm/core/core.scala
+++ jvm/src/main/scala/org/eigengo/cm/core/core.scala
@@ -4,15 +4,35 @@ import akka.actor.{ Props, ActorSystem }
 import com.rabbitmq.client.ConnectionFactory
 import com.github.sstone.amqp.ConnectionOwner
 
-trait Core {
+trait CoreConfiguration {
 
+  def amqpConnectionFactory: ConnectionFactory
+
+}
+
+trait ConfigCoreConfiguration extends CoreConfiguration {
+  def system: ActorSystem
+
+  // connection factory
+  lazy val amqpConnectionFactory = {
+    val amqpHost = system.settings.config.getString("cm.amqp.host")
+    val cf = new ConnectionFactory()
+    cf.setHost(amqpHost)
+    cf
+  }
+
+}
+
+trait Core {
+  this: CoreConfiguration =>
+
+  // start the actor system
   implicit lazy val system = ActorSystem("recog")
 
-  val amqpConnectionFactory = new ConnectionFactory()
-  amqpConnectionFactory.setHost("localhost")
-
+  // create a "connection owner" actor, which will try and reconnect automatically if the connection ins lost
   lazy val amqpConnection = system.actorOf(Props(new ConnectionOwner(amqpConnectionFactory)))
 
+  // create the coordinator actor
   lazy val coordinator = system.actorOf(Props(new CoordinatorActor(amqpConnection)), "coordinator")
 
 }
