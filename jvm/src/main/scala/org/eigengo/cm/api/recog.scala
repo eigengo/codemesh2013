d415c5bd536d3eac4c908e4c9500b76c4414832e
Jan Machacek <jan.machacek@gmail.com>
2013-12-04 17:03:07 +0000

    Recog service

--- jvm/src/main/scala/org/eigengo/cm/api/recog.scala
+++ jvm/src/main/scala/org/eigengo/cm/api/recog.scala
@@ -1,6 +1,6 @@
 package org.eigengo.cm.api
 
-import akka.actor.{ Actor, ActorRef }
+import akka.actor.{Props, Actor, ActorRef}
 import spray.http._
 import spray.http.HttpResponse
 import scala.util.Success
@@ -28,11 +28,53 @@ class RecogService(coordinator: ActorRef) extends Actor {
 
   implicit val timeout = akka.util.Timeout(2.seconds)
 
-  def receive: Receive = ???
+  def receive: Receive = {
+    // clients get connected to self (singleton handler)
+    case _: Http.Connected =>
+      sender ! Http.Register(self)
+
+    // POST to /recog/...
+    case HttpRequest(HttpMethods.POST, uri, _, entity, _) =>
+      val client = sender
+      uri.path.toString() match {
+        case RootUri =>
+          (coordinator ? Begin(1)).mapTo[String].onComplete {
+            case Success(sessionId) => client ! HttpResponse(entity = sessionId)
+            case Failure(ex) => client ! HttpResponse(entity = ex.getMessage, status = StatusCodes.InternalServerError)
+          }
+        case StaticUri(sessionId) =>
+          coordinator ! SingleImage(sessionId, entity.data.toByteArray)
+      }
+
+    // stream begin to /recog/[h264|mjpeg]/:id
+    case ChunkedRequestStart(HttpRequest(HttpMethods.POST, uri, _, entity, _)) =>
+      val streamer = uri.path.toString() match {
+        case MJPEGUri(sessionId) => context.actorOf(Props(new StreamingRecogService(coordinator, sessionId, SingleImage)))
+        case H264Uri(sessionId)  => context.actorOf(Props(new StreamingRecogService(coordinator, sessionId, FrameChunk)))
+      }
+      sender ! RegisterChunkHandler(streamer)
+
+    // all other requests
+    case HttpRequest(method, uri, _, _, _) =>
+      sender ! HttpResponse(entity = s"No such endpoint $method at $uri. That's all we know.", status = StatusCodes.NotFound)
+  }
 
 }
 
 class StreamingRecogService[A](coordinator: ActorRef, sessionId: String, message: (String, Array[Byte]) => A) extends Actor {
-  def receive: Receive = ???
+
+  def receive = {
+    // stream mid to /recog/[h264|mjpeg]/:id; see above ^
+    case MessageChunk(data, _) =>
+      // our work is done: bang it to the coordinator.
+      coordinator ! message(sessionId, data.toByteArray)
+
+    // stream end to /recog/[h264|mjpeg]/:id; see above ^^
+    case ChunkedMessageEnd(_, _) =>
+      // we say nothing back
+      sender ! HttpResponse(entity = "{}")
+      context.stop(self)
+  }
+
 }
 
